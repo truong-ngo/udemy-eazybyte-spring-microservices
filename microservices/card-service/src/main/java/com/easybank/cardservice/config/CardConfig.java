da1454dd1e971852e3962c0117ccfb3921c23a6d
truong-ngo <truong.ngo1993@gmail.com>
2023-06-22 00:03:49 +0700

    add

---
+++ microservices/card-service/src/main/java/com/easybank/cardservice/config/CardConfig.java
@@ -0,0 +1,18 @@
+package com.easybank.cardservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "loans")
+@Data
+public class CardConfig {
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+    private List<String> activeBranches;
+}
