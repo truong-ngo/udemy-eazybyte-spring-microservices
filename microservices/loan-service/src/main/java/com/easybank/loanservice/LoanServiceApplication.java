d70d74ec59fe73cf51deb96d0f093b7f4910a17c
truong-ngo <truong.ngo1993@gmail.com>
2023-06-25 00:12:09 +0700

    add

--- microservices/loan-service/src/main/java/com/easybank/loanservice/LoanServiceApplication.java
+++ microservices/loan-service/src/main/java/com/easybank/loanservice/LoanServiceApplication.java
@@ -2,12 +2,14 @@ package com.easybank.loanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @RefreshScope
+@EnableDiscoveryClient
 public class LoanServiceApplication {
 
 	public static void main(String[] args) {
