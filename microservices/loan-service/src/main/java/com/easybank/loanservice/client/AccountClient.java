cfc2362e7671c5c236d3f1893ff1ec188e7e10ce
truong-ngo <truong.ngo1993@gmail.com>
2023-06-25 01:51:00 +0700

    add

--- microservices/loan-service/src/main/java/com/easybank/loanservice/client/AccountClient.java
+++ microservices/loan-service/src/main/java/com/easybank/loanservice/client/AccountClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "accounts", url = "accounts:8080")
+@FeignClient("accounts")
 public interface AccountClient {
     @GetMapping("/customers/{id}")
     CustomerDTO getCustomer(@PathVariable Long id);
