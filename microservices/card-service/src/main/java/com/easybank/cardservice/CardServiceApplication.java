f5c991a650e9b12cd3433f410126adaeea26d56c
truong-ngo <truong.ngo1993@gmail.com>
2023-06-25 00:14:54 +0700

    add

--- microservices/card-service/src/main/java/com/easybank/cardservice/CardServiceApplication.java
+++ microservices/card-service/src/main/java/com/easybank/cardservice/CardServiceApplication.java
@@ -2,12 +2,14 @@ package com.easybank.cardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @RefreshScope
+@EnableDiscoveryClient
 public class CardServiceApplication {
 
 	public static void main(String[] args) {
