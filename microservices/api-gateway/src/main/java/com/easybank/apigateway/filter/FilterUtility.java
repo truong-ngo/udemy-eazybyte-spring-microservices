0fa3773e2086d15fbdea81427956f24e633b5d8a
truong-ngo <truong.ngo1993@gmail.com>
2023-06-25 21:04:04 +0700

    add

--- microservices/api-gateway/src/main/java/com/easybank/apigateway/filter/FilterUtility.java
+++ microservices/api-gateway/src/main/java/com/easybank/apigateway/filter/FilterUtility.java
@@ -1,2 +1,29 @@
-package com.easybank.apigateway.filter;public class FilterUtility {
+package com.easybank.apigateway.filter;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.List;
+
+@Component
+public class FilterUtility {
+    public static final String CORRELATION_ID = "eazybank-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeader) {
+        if (requestHeader != null && requestHeader.get(CORRELATION_ID) != null) {
+            List<String> requestHeaderList = requestHeader.get(CORRELATION_ID);
+            return requestHeaderList != null ? requestHeaderList.stream().findFirst().orElse(null) : null;
+        } else {
+            return null;
+        }
+    }
+
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value) {
+        return exchange.mutate().request(exchange.getRequest().mutate().header(name, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return this.setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
 }
