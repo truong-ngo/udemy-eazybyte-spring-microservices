cc37773e911d1f2e1077fb839f69430b7157fe62
truong-ngo <truong.ngo1993@gmail.com>
2023-06-26 15:42:57 +0700

    add

--- microservices/api-gateway/src/main/java/com/easybank/apigateway/trace/logging/ObservationContextSnapshotLifter.java
+++ microservices/api-gateway/src/main/java/com/easybank/apigateway/trace/logging/ObservationContextSnapshotLifter.java
@@ -1,2 +1,88 @@
-package com.easybank.apigateway.trace.logging;public class ObservationContextSnapshotLifter {
+package com.easybank.apigateway.trace.logging;
+
+import io.micrometer.context.ContextRegistry;
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.context.ThreadLocalAccessor;
+import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
+import org.reactivestreams.Subscription;
+import org.springframework.lang.Nullable;
+import reactor.core.CoreSubscriber;
+import reactor.core.Scannable;
+import reactor.util.context.Context;
+
+import java.util.function.BiFunction;
+
+public class ObservationContextSnapshotLifter<T> implements CoreSubscriber<T> {
+
+    public static <T> BiFunction<Scannable, CoreSubscriber<? super T>, CoreSubscriber<? super T>> lifter() {
+        return (scannable, coreSubscriber) -> new ObservationContextSnapshotLifter<>(coreSubscriber);
+    }
+
+    private final CoreSubscriber<? super T> delegate;
+
+    @Nullable
+    private final ObservationThreadLocalAccessor observationThreadLocalAccessor;
+
+    public ObservationContextSnapshotLifter(CoreSubscriber<? super T> delegate) {
+        this.delegate = delegate;
+        this.observationThreadLocalAccessor = findObservationThreadLocalAccessor();
+    }
+
+    @Nullable
+    private static ObservationThreadLocalAccessor findObservationThreadLocalAccessor() {
+        for (ThreadLocalAccessor<?> threadLocalAccessor : ContextRegistry.getInstance().getThreadLocalAccessors()) {
+            if (ObservationThreadLocalAccessor.KEY.equals(threadLocalAccessor.key())
+                    && threadLocalAccessor instanceof ObservationThreadLocalAccessor observationThreadLocalAccessor) {
+                return observationThreadLocalAccessor;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Context currentContext() {
+        return delegate.currentContext();
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        delegate.onSubscribe(subscription);
+    }
+
+    @Override
+    public void onNext(T t) {
+        if (isObservationThreadLocalUnset()) {
+            try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(currentContext(), ObservationThreadLocalAccessor.KEY)) {
+                delegate.onNext(t);
+            }
+        } else {
+            delegate.onNext(t);
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        if (isObservationThreadLocalUnset()) {
+            try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(currentContext(), ObservationThreadLocalAccessor.KEY)) {
+                delegate.onError(throwable);
+            }
+        } else {
+            delegate.onError(throwable);
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        if (isObservationThreadLocalUnset()) {
+            try (ContextSnapshot.Scope scope = ContextSnapshot.setThreadLocalsFrom(currentContext(), ObservationThreadLocalAccessor.KEY)) {
+                delegate.onComplete();
+            }
+        } else {
+            delegate.onComplete();
+        }
+    }
+
+    private boolean isObservationThreadLocalUnset() {
+        return observationThreadLocalAccessor != null && observationThreadLocalAccessor.getValue() == null;
+    }
 }
