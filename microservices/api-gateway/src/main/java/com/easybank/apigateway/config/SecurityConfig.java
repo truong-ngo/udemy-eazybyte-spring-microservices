1e95404372688dec17b664fc2d5e47e59fc0eb82
truong-ngo <truong.ngo1993@gmail.com>
2023-07-05 23:54:17 +0700

    add

--- microservices/api-gateway/src/main/java/com/easybank/apigateway/config/SecurityConfig.java
+++ microservices/api-gateway/src/main/java/com/easybank/apigateway/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.easybank.apigateway.config;public class SecurityConfig {
+package com.easybank.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/accounts/**").authenticated()
+                        .pathMatchers("/cards/**").authenticated()
+                        .pathMatchers("/loans/**").permitAll()
+                        .pathMatchers("/**").permitAll())
+                .oauth2ResourceServer().jwt();
+        http.csrf().disable();
+        return http.build();
+    }
 }
