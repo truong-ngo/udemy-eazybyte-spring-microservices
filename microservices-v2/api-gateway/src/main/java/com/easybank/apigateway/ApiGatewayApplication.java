5355c44dab115a9d9785ffcfe49f1bcdac0a15e6
truong-ngo <truong.ngo1993@gmail.com>
2023-07-15 15:12:36 +0700

    add

--- microservices-v2/api-gateway/src/main/java/com/easybank/apigateway/ApiGatewayApplication.java
+++ microservices-v2/api-gateway/src/main/java/com/easybank/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,15 @@ package com.easybank.apigateway;
 
 import com.easybank.apigateway.trace.logging.ObservationContextSnapshotLifter;
 import io.micrometer.context.ContextSnapshot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -29,5 +33,4 @@ public class ApiGatewayApplication {
 				ObservationContextSnapshotLifter.class.getSimpleName(),
 				Operators.lift(ObservationContextSnapshotLifter.lifter()));
 	}
-
 }
